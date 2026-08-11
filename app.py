971a68345c29a07aa69085dbc5a475a616bf06fb
anmolkumar-dev-sudo <anmolofficial89@gmail.com>
2026-08-11 16:32:05 +0530

    Configure CORS for specific origins
    Updated CORS middleware to allow specific origins.

--- app.py
+++ app.py
@@ -25,11 +25,16 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://reveal-1-4vvr.onrender.com",
+        "http://127.0.0.1:8000",
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 pwd_context = CryptContext(
     schemes=["bcrypt"],
     deprecated="auto"
